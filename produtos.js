977353b3683ff06d027b3a2b56280a074b7f94da
williammelo56 <vendas1@viapaineis.com.br>
2025-10-07 11:06:59 -0300

    Update produtos.js

--- produtos.js
+++ produtos.js
@@ -1,7 +1,7 @@
 const produtosPainel = [
   // --- Gabinetes ---
   { nome: "P2.97 Rental", resolucao: "P2.97", dimensao: "0.50x1.00", valor: "4500.00", tipo: "gabinete", ambiente: "outdoor" },
-  { nome: "P3.91 Rental", resolucao: "P3.91", dimensao: "0.50x1.00", valor: "3900.00", tipo: "gabinete", ambiente: "outdoor" },
+  { nome: "P3.9 Rental", resolucao: "P3.9", dimensao: "0.50x1.00", valor: "3900.00", tipo: "gabinete", ambiente: "outdoor" },
   { nome: "P4.81 Rental", resolucao: "P4.81", dimensao: "0.50x0.50", valor: "1750.00", tipo: "gabinete", ambiente: "outdoor" },
   { nome: "P5 Magnésio", resolucao: "P5", dimensao: "0.96x0.96", valor: "6600.00", tipo: "gabinete", ambiente: "outdoor" },
 
@@ -15,4 +15,5 @@ const produtosPainel = [
   { nome: "P3 IMÃ", resolucao: "P3", dimensao: "0.32x0.16", valor: "10350.00", tipo: "m2", ambiente: "outdoor" },
   { nome: "P4 IMÃ", resolucao: "P4", dimensao: "0.32x0.16", valor: "6300.00", tipo: "m2", ambiente: "indoor" },
   { nome: "P5 IMÃ", resolucao: "P5", dimensao: "0.32x0.16", valor: "8430.00", tipo: "m2", ambiente: "outdoor" },
+
 ];
